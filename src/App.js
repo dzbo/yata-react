e4aa07b0aceba3c5474ce773ffcbd4bb86b14d05
Dominik Zborowski <dominik.zborowski@gmail.com>
2020-03-19 00:09:34 +0100

    Add react-intl

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {IntlProvider} from 'react-intl';
+import deDeTranslations from './translations/de_DE.json';
+import enUsTranslations from './translations/en_US.json';
+import {FormattedMessage} from 'react-intl';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(...args) {
+    super(...args);
+
+    // set defaults, in real app this could be taken from browser locale
+    this.state = {
+      locale: "en-US",
+      messages: enUsTranslations
+    };
+  }
+
+  handleEnClick = () => {
+    this.setState({ locale: "en-US", messages: enUsTranslations });
+  }
+
+  handleDeClick = () => {
+    this.setState({ locale: "de-DE", messages: deDeTranslations });
+  }
+
+  render() {
+    return (
+      <IntlProvider locale={this.state.locale} messages={this.state.messages}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>
+              <FormattedMessage id="pages_home_header" />
+            </h1>
+            <p>
+              <FormattedMessage id="pages_home_description" values={{ framework: 'React' }} />
+            </p>
+            <div>
+              <a href="#!" onClick={this.handleEnClick}>EN</a>
+              |
+              <a href="#!" onClick={this.handleDeClick}>DE</a>
+            </div>
+          </header>
+        </div>
+      </IntlProvider>
+    )
+  }
 }
-
-export default App;
